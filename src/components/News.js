b13ccaa0e359280496a57e5a899984274af294ba
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-06-12 14:36:39 +0530

    Fetching API Key from NewsAPI

--- src/components/News.js
+++ src/components/News.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        News
-        <NewsItem/>
-        <NewsItem/><NewsItem/>
-        <NewsItem/><NewsItem/><NewsItem/>
-        <NewsItem/><NewsItem/><NewsItem/><NewsItem/>
-        <NewsItem/><NewsItem/><NewsItem/>
-        <NewsItem/><NewsItem/>
-        <NewsItem/>
-      </div>
-    )
+      <div className="container mx-2 my-2">
+        <h2>Top Headlines</h2>
+        <div className="row mx-2 my-2">
+          <div className="col-md-4">
+            <NewsItem title="my title" description="desc" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title2" description="des2c" />
+          </div>
+          <div className="col-md-4">
+            <NewsItem title="my title3" description="d3esc" />
+          </div>
+        </div>
+        </div>
+    );
   }
 }
 
-export default News
+export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description }=this.props;
     return (
       <div>
-        NewsItem
+        <div className="card" style={{ width: "18rem" }}>
+          <img src="https://c.ndtvimg.com/2023-06/9eust2bo_r_625x300_10_June_23.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=675" className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description} </p>
+            <a href="/newsdetail" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
