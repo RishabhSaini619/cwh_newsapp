f7c40e2c50a4fa533839e3ca2f521105cb5ba73e
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-06-12 14:43:34 +0530

    test probs

--- src/components/News.js
+++ src/components/News.js
@@ -8,7 +8,7 @@ export class News extends Component {
         <h2>Top Headlines</h2>
         <div className="row mx-2 my-2">
           <div className="col-md-4">
-            <NewsItem title="my title" description="desc" />
+            <NewsItem title="my title" description="desc" imageUrl="https://c.ndtvimg.com/2023-06/9eust2bo_r_625x300_10_June_23.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=675" />
           </div>
           <div className="col-md-4">
             <NewsItem title="my title2" description="des2c" />
