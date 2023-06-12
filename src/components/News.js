85316b4abb129eedae1bdcb3240b4da12fd0557e
rishabhsaini619 <rishabhsaini619@gmail.com>
2023-06-12 16:25:05 +0530

    Using Fetch API in React to populate NewsItems

--- src/components/News.js
+++ src/components/News.js
@@ -221,7 +221,7 @@ export class News extends Component {
       content:
         "Still from a video shared by Netflix. (courtesy: netflix_in)\r\nHarman Baweja is back and this time, it looks like he is here to stay. The actor who made his debut in 2008 with the Priyanka Chopra film… [+2249 chars]",
     },
-  
+
     {
       source: {
         id: null,
@@ -327,19 +327,37 @@ export class News extends Component {
       loadinf: false,
     };
   }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=42d65406e6d940c7a2d04f236e8ef836";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    this.setState({ articles: parsedData.articles });
+    console.log(parsedData);
+  }
+
   render() {
     return (
-      <div className="container mx-2 my-2">
+      <div className="container mx-4 my-4">
         <h2>Top Headlines</h2>
-        <div className="row mx-2 my-2">
+        <div className="row mx-5 my-3">
           {this.state.articles.map((element) => {
             return (
               <div className="col-md-4" key={element.url}>
                 <NewsItem
-                  title={element.title.slice(0, 40)}
-                  description={element.description.slice(0, 90)}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
+                  title={element.title ? element.title.slice(0, 40) : ""}
+                  description={
+                    element.description
+                      ? element.description.slice(0, 90)
+                      : "Read More"
+                  }
+                  imageUrl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://images.hindustantimes.com/tech/img/2023/06/11/1600x900/sunspectrum_mpso_960_1686469966200_1686469974791.jpg"
+                  }
+                  newsUrl={element.url ? element.url : ""}
                 />
               </div>
             );
